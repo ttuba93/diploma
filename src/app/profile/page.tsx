ac92f280993ec8d9024ebabe080249c554ac5e2c
Tuba Abdugapparova <t_abdugapparova@kbtu.kz>
2025-04-03 17:47:59 +0500

    login

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -152,7 +152,7 @@ export default function UserProfile() {
                   <br />
                   <Text strong>Phone:</Text> <Text>{(profile as Student).telephone_number || "N/A"}</Text>
                 </>
-              ) : (
+              ) : user?.role === "dean manager" ? (
                 <>
                   <Text strong>Name:</Text> <Text>{(profile as ManagerProfile).first_name} {(profile as ManagerProfile).last_name}</Text>
                   <br />
@@ -168,7 +168,7 @@ export default function UserProfile() {
                   <br />
                   <Text strong>Position:</Text> <Text>{(profile as ManagerProfile).position}</Text>
                 </>
-              )}
+              ) : null}
             </div>
           ) : (
             <Alert 
